623dc1b31271364669d3b1653a5caabdf433776c
campusboy <campusboy87@gmail.com>
2019-01-12 05:16:31 +0300

    init

---
+++ mouse-based-parallax-sunset.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Mouse-Based-Parallax Sunset
+ */
+
+add_action( 'login_header', 'mbps_add_template' );
+add_action( 'login_enqueue_scripts', 'mbps_enqueue_assets' );
+
+/**
+ * Подключает шаблон.
+ *
+ * @return void
+ */
+function mbps_add_template() {
+    include __DIR__ . '/template.php';
+}
+
+/**
+ * Подключает JS и CSS плагина.
+ *
+ * @return void
+ */
+function mbps_enqueue_assets() {
+    wp_enqueue_script( 'mbps-scripts', plugins_url( 'assets/scripts.js', __FILE__ ) );
+    wp_enqueue_style( 'mbps-styles', plugins_url( 'assets/styles.css', __FILE__ ) );
+}
---
+++ template.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Шаблон плагина
+ *
+ * Код взят с https://codepen.io/quinlo/pen/XPVmVj
+ */
+?>
+<div class="totalcontainer">
+    <div class="laya-please layer-1">
+    </div>
+    <div class="laya-please layer-2">
+    </div>
+    <div class="container1">
+        <div class="laya-please layer-3">
+        </div>
+        <div class="laya-please layer-4">
+        </div>
+        <div class="laya-please layer-5">
+        </div>
+        <div class="laya-please layer-6">
+        </div>
+    </div>
+    <div class="container2">
+        <div class="laya-please layer-7">
+        </div>
+        <div class="laya-please layer-8">
+        </div>
+    </div>
+</div>
